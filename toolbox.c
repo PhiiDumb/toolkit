a216f3f3320f52f47e2bc7f5185dc7b33a04f143
PhiiDumb <124185965+PhiiDumb@users.noreply.github.com>
2023-02-15 16:19:07 +0100

    Now based on C

---
+++ toolbox.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main() {
+
+   system("color 0e");
+    system("title Alphii's All-In-One Discord Toolbox");
+    system("cls");
+
+
+    printf("    _____  .____   __________  ___ ___ .___.___ \n");
+    printf("  /  _  \\ |    |  \\______   \\/   |   \\|   |   |\n");
+    printf(" /  /_\\  \\|    |   |     ___/    ~    \\   |   |\n");
+    printf("/    |    \\    |___|    |   \\    Y    /   |   |\n");
+    printf("\\____|__  /_______ \\____|    \\___|_  /|___|___|\n");
+    printf("        \\/        \\/               \\/          \n");
+    printf("by Alphii_ on YT\n");
+    printf("Checking if you have subscribed and joined the discord.\n");
+    system("timeout 1 >nul");
+    system("cls");
+    
+    
+    
+    printf("    _____  .____   __________  ___ ___ .___.___ \n");
+    printf("  /  _  \\ |    |  \\______   \\/   |   \\|   |   |\n");
+    printf(" /  /_\\  \\|    |   |     ___/    ~    \\   |   |\n");
+    printf("/    |    \\    |___|    |   \\    Y    /   |   |\n");
+    printf("\\____|__  /_______ \\____|    \\___|_  /|___|___|\n");
+    printf("        \\/        \\/               \\/          \n");
+    printf("by Alphii_ on YT\n");
+    printf("Checking if you have subscribed and joined the discord..\n");
+    system("timeout 1 >nul");
+    system("cls");
+    
+    
+    printf("    _____  .____   __________  ___ ___ .___.___ \n");
+    printf("  /  _  \\ |    |  \\______   \\/   |   \\|   |   |\n");
+    printf(" /  /_\\  \\|    |   |     ___/    ~    \\   |   |\n");
+    printf("/    |    \\    |___|    |   \\    Y    /   |   |\n");
+    printf("\\____|__  /_______ \\____|    \\___|_  /|___|___|\n");
+    printf("        \\/        \\/               \\/          \n");
+    printf("by Alphii_ on YT\n");
+    printf("Checking if you have subscribed and joined the discord...\n");
+    system("timeout 1 >nul");
+    system("cls");
+    
+    
+    
+    
+    printf("    _____  .____   __________  ___ ___ .___.___ \n");
+    printf("  /  _  \\ |    |  \\______   \\/   |   \\|   |   |\n");
+    printf(" /  /_\\  \\|    |   |     ___/    ~    \\   |   |\n");
+    printf("/    |    \\    |___|    |   \\    Y    /   |   |\n");
+    printf("\\____|__  /_______ \\____|    \\___|_  /|___|___|\n");
+    printf("        \\/        \\/               \\/          \n");
+     printf("Discord Toolbox\nby Alphii_ on YT\n\n");
+    printf("[1] Autotyper              [2] Fake Nitro\n");
+    printf("[3] Animated Emojis        [4] Temp Mail\n");
+    printf("Enter which tool you'd like to choose: ");
+
+    int choice;
+    scanf("%d", &choice);
+
+    if (choice == 1) {
+        printf("Just a sec.\n");
+        system("start https://dl.mediafire.com/file/9myj8xwcjsc269o/autotyper.exe/file");
+    } else if (choice == 2) {
+        printf("Here's your fake Nitro Code: https://discord.gift/jpVXgSCdjbmW9S5NjVRnX2Sg\n");
+        printf("DO NOT COPY PASTE INTO BROWSER IT WON'T WORK. Instead copy paste into discord dm/server.\n");
+    } else if (choice == 3) {
+        printf("Opening website for Animated Emojis Without Nitro\n");
+        system("start https://imageclipboard.com");
+    } else if (choice == 4) {
+        printf("Starting temp mail website...\n");
+        system("start https://temp-mail.org");
+    } else {
+        printf("Invalid choice\n");
+    }
+
+    return 0;
+    
+}
